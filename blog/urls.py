16353cc7faa88f95309e8911dd564e9b5ee6a9c5
Michael Gauthier <mike@silverorange.com>
2026-03-20 17:58:35 -0300

    Add basic view and template for post index

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,5 @@ from . import views
 
 urlpatterns = [
     path("posts/", views.index, name="index"),
-    path("post/<uuid:id>/", views.detail, name="detail"),
     path("", views.welcome, name="welcome"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,9 @@
-from uuid import UUID
-
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "blog/index.html")
-
-
-def detail(request: HttpRequest, pk: UUID) -> HttpResponse:
-    return render(request, "blog/detail.html")
 
 
 def welcome(request: HttpRequest) -> HttpResponse:
